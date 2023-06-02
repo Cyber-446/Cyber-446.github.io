da0336321a06411c77b72442ec4f7db21271b4a5
Cyber-446 <76432428+Cyber-446@users.noreply.github.com>
2023-06-02 09:14:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let StartFunc = 0;
+		document.getElementById("startbut").addEventListener("click", pressedStart);
+		document.getElementById("stopbut").addEventListener("click", pressedStop);
+		function pressedStart(){
+		StartFunc=setInterval(myFunction, 1000)
+		}
+		function pressedStop(){
+		clearInterval(StartFunc)
+		}
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+			function myFunction() {
+                (async () => {
+                    let response = await fetch('https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1');
+                    let el = document.getElementById('demo')
+                    el.innerHTML = ""
+                    let text = await response.text(); // прочитать тело ответа как текст
+                    for (const [key, value] of Object.entries(JSON.parse(text)[0])) {
+                        const newEl = document.createElement("li")
+                        newEl.appendChild(document.createTextNode(`${key}: ${value}`))
+                        el.appendChild(newEl)
+                        await sleep(100)
+                    }
+                })()
+			}
