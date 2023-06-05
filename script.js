16388875558181a309b944c0d8bcb67e3d58fde4
Cyber-446 <76432428+Cyber-446@users.noreply.github.com>
2023-06-05 15:22:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ let StartFunc = 0;
             }
 			function myFunction() {
                 (async () => {
-                    let response = await fetch('https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1');
+                    //let response = await fetch('https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1');
+		    let response = await fetch('http://127.0.0.1:1880/#flow/21683101148a1568');
                     let el = document.getElementById('demo')
                     el.innerHTML = ""
                     let text = await response.text(); // прочитать тело ответа как текст
